7eb1130cbef44007c91193eb91167992a5a552ec
TomTeisman <tjg.teisman@gmail.com>
2025-09-22 11:19:45 +0200

    added the username to the database table

--- database/scores_table.sql
+++ database/scores_table.sql
@@ -1,9 +1,3 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `scores`
 --
@@ -12,6 +6,7 @@ DROP TABLE IF EXISTS `scores`;
 CREATE TABLE IF NOT EXISTS `scores` (
   `id` int NOT NULL AUTO_INCREMENT,
   `score` int NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
